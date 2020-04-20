cc4cb884917c696e05e6992a7692cfc804a3b6fb
cyx01293 <42715452+cyx01293@users.noreply.github.com>
2020-04-20 03:13:25 -0400

    1

---
+++ project/projTask1/c2/DFSSERVER.java
@@ -0,0 +1,32 @@
+//package dfs;
+
+import java.util.*;
+
+
+import java.io.*;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.rmi.*;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+
+public class DFSSERVER {
+	public static void main(String[] args) throws UnknownHostException, RemoteException {
+		// TODO Auto-generated method stub
+		InetAddress inetAddress = InetAddress. getLocalHost();
+		System.out.println("IP Address:- " + inetAddress. getHostAddress());
+		int port = 1099;
+		System.out.println("Port:- " + Integer.toString(port));
+		SERVICE serv = new SERVICE(inetAddress. getHostAddress() + ":" + port);
+		//Transmit this service to remote service interface
+        Storage skeleton = (Storage) UnicastRemoteObject.exportObject(serv, 0);
+        //Register the service to port 1099
+        Registry registry = LocateRegistry.createRegistry(port);
+        // Register this service, and assign "DFS" to it
+        registry.rebind("DFS" + inetAddress. getHostAddress() + ":" + port, skeleton);
+        System.out.println("This server is connected:" + serv.isConnected);
+        
+	}
+
+}
