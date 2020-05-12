ca2b272c1543afea0676b72b3d1c72d311cd9c0a
cyx01293 <42715452+cyx01293@users.noreply.github.com>
2020-05-11 20:44:58 -0400

    3

--- SORT.java
+++
@@ -1,8 +0,0 @@
-//package RPC;
-
-import java.rmi.*; 
-import java.rmi.server.*; 
-
-public interface SORT extends Remote {
-	public int[] sortQuery(int[] unsorted, int N, int R) throws RemoteException, InterruptedException;
-}
